88c65971499756388620e7b1d86034dd023e3f30
Mugisha Clinton <mugishaclinton412@gmail.com>
2023-10-16 03:32:47 +0300

    when multiplying 2*2

--- test/test.js
+++ test/test.js
@@ -5,6 +5,9 @@ describe("Multiply Function", () => {
   it("should return 1 when multiplying 1 and 1", () => {
     assert.equal(multiply(1, 1), 1);
   });
+  it("should return 4 when multiplying 2 and 2", () => {
+    assert.equal(multiply(2, 2), 4);
+  });
 
   
 });
